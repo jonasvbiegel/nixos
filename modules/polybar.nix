ba968f2abf9d5d68b2f43f0e4738abcbecec95e3
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-06-27 03:52:43 +0200

    Added battery, wifi and volume modules to polybar

--- modules/polybar.nix
+++ modules/polybar.nix
@@ -7,26 +7,30 @@
   home.packages = [pkgs.nerd-fonts.caskaydia-mono];
 
   services.polybar = {
+    # this makes pulseaudio work
+    package = pkgs.polybar.override {pulseSupport = true;};
     enable = true;
     script = "polybar polybar &";
     settings = {
       "bar/polybar" = {
         monitor = "eDP-1";
         width = "100%";
-        height = "4%";
+        height = "3%";
 
         background = "#AA000000";
 
-        # border-bottom-size = 2;
-        # border-bottom-color = "#7aa2f7";
+        # border-right-size = 2;
+        # border-right-color = "#AA000000";
+
+        module-margin = 1;
 
         font-0 = "CaskaydiaMono Nerd Font:size=16:weight=500;2";
         font-1 = "CaskaydiaMono Nerd Font:size=16:weight=100;2";
         font-2 = "CaskaydiaMono Nerd Font:size=16:weight=bold;2";
 
-        modules-left = "xworkspaces systray";
-        modules-center = "xwindow";
-        modules-right = "time";
+        modules-left = "xworkspaces";
+        # modules-center = "xwindow";
+        modules-right = "systray pulseaudio wireless-network battery time";
       };
 
       "module/xwindow" = {
@@ -70,10 +74,49 @@
         date = "%H:%M";
 
         label = "%date%";
-        label-padding = 2;
+        label-padding = "13pt";
 
         format-prefix = "";
         format-prefix-font = 2;
+      };
+
+      "module/battery" = {
+        type = "internal/battery";
+
+        battery = "BAT1";
+        adapter = "ACAD";
+
+        low-at = 10;
+
+        label-charging = "󰁹 %percentage%%";
+        label-charging-spacing = 2;
+        label-discharging = "󰁹 %percentage%%";
+        label-discharging-spacing = 2;
+      };
+
+      "module/wireless-network" = {
+        type = "internal/network";
+        interface = "wlp1s0";
+
+        format-connected = "󰖩  <label-connected>";
+        format-disconnected = "󰖪  <label-disconnected>";
+
+        label-connected = "%essid%";
+        label-disconnected = "disconnected";
+      };
+
+      "module/pulseaudio" = {
+        type = "internal/pulseaudio";
+
+        sink = "alsa_output.pci-0000_c1_00.6.analog-stereo";
+
+        format-volume = "<label-volume>";
+        format-muted = "<label-muted>";
+
+        label-volume = "  %percentage%%";
+
+        label-muted = "  %percentage%%";
+        label-muted-foreground = "#FF888888";
       };
 
       "settings" = {
